962ae640e8af99e4c36d1a9fdfeeedfbdfb7c26e
Michael Smith <m90smith@gmail.com>
2013-10-22 23:14:25 -0600

    Implemented getUsers from searching the alphabet

--- twitter.php
+++ twitter.php
@@ -1,5 +1,12 @@
 <?php
     
+
+    $letters = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+    
+    $numberOfUsersPerLetter = 3;
+    $stepBetweenUsers = 3;
+    
+    $users = array();
     require_once('./TwitterAPIWrapper.php');
     
     $settings = array(
@@ -9,17 +16,73 @@
                       'consumer_secret' => "ujY0ijb9hNex8OgxcCv3p8Eqpnoyhg0VirbiPr9I"
                       );
     
-    
-    $url = 'https://api.twitter.com/1.1/users/lookup.json';
-    $getfield = '?user_id=783214,6483922,746843,6354349';
-    
-//    $url = 'https://api.twitter.com/1.1/users/search.json';
-//    $getfield = '?q=a';
-    
-    $requestMethod = 'GET';
-    
     $twitter = new TwitterAPIExchange($settings);
-    echo $twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest();
+    
+////    $testURL = 'https://api.twitter.com/1.1/users/lookup.json';
+////    $testgetfield = '?user_id=783214,6483922,746843,6354349';
+//    $testURL = 'https://api.twitter.com/1.1/users/search.json';
+//    $testgetfield = '?q=A&count=1&page=2';
+//    echo $twitter->setGetfield($testgetfield)->buildOauth($testURL, 'GET')->performRequest();
+
+    // lookup users for each letter
+    $usersToRequest = $numberOfUsersPerLetter * $stepBetweenUsers;
+    
+    getUsers();
+    
+    function getUsers(){
+        global $letters,$twitter,$usersToRequest,$numberOfUsersPerLetter,$stepBetweenUsers;
+        
+        $users = array();
+        foreach($letters as $letter) {
+            
+            $usersForLetter = getUsersForLetter($twitter, $letter,$usersToRequest);
+            $selectedUsers = selectUsersByStep($usersForLetter,$numberOfUsersPerLetter, $stepBetweenUsers);
+           $users = array_merge((array)$users,(array)$selectedUsers);
+        }
+        
+        echo 'Retrieved '.count($users).' users from searching by letters'."\n";
+        return $users;
+    }
+    
+        
     
     
+    function getUsersForLetter($requestObject, $letter, $usersToRequest) {
+        echo 'Requesting '.$usersToRequest.': '.$letter.' ...';
+        $searchURL = 'https://api.twitter.com/1.1/users/search.json';
+        $requestMethod = 'GET';
+        $resultsForLetter = array();
+        
+        for ($page = 0; $page < 50; $page++){
+            $getfield = '?q='.$letter.'&count=20'.'&page='.$page;
+            $result = json_decode($requestObject->setGetfield($getfield)->buildOauth($searchURL, $requestMethod)->performRequest());
+            $resultsForLetter = array_merge((array)$resultsForLetter, (array)$result);
+            if(count($resultsForLetter) >= $usersToRequest){
+                break;
+            }
+        }
+        
+        echo 'Received '.count($resultsForLetter).' Users'."\n";
+        
+        return $resultsForLetter;
+    }
+    
+    function selectUsersByStep($users,$numOfUsersToSelect, $step){
+        
+        $numOfUsersNeeded = $numOfUsersToSelect*$step;
+        if (count($users)<$numOfUsersNeeded) {
+            echo "Not Enough Users to Select From";
+            return;
+        }
+        
+        $selectedUsers = array();
+        for($i=0;$i<$numOfUsersNeeded;$i = $i+$step){
+            array_push($selectedUsers,$users[$i]);
+        }
+        return $selectedUsers;
+    }
+    
+    function printUsers($array) {
+        print_r($array);
+    }
 ?>
